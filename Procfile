812342e25f71e655b82f050287650be50fefdd95
Jean D'Amore <jdamore@thoughtworks.com>
2016-04-02 19:15:32 +1000

    Trying to fix deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./go-init.sh && ./go-api.sh start
+web: ./go-init.sh
--- go-init.sh
+++ go-init.sh
@@ -4,3 +4,6 @@ rm -rf node_modules
 npm install
 rm -rf coffee-api-challenge
 git clone https://github.com/software-shokunin/coffee-api-challenge.git
+pushd coffee-api-challenge
+ls -la
+popd
