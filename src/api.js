0a3742eb924e4ab7f74ec1b564e38baef69cb0b3
Jean D'Amore <jdamore@thoughtworks.com>
2016-04-02 23:07:34 +1000

    Fixed indentation

--- src/api.js
+++ src/api.js
@@ -5,43 +5,43 @@ import ContractReader from './contract-reader';
 
 class Api {
 
-  constructor(port) {
-    this.contractReader = new ContractReader('./coffee-api-challenge/contracts');
-    this.port = port;
-    this.app = App();
-    this.response = (contract) => this._response(contract);
-    this.routes = () => this._routes();
-    this.start = () => this._start();
-  }
+	constructor(port) {
+		this.contractReader = new ContractReader('./coffee-api-challenge/contracts');
+		this.port = port;
+		this.app = App();
+		this.response = (contract) => this._response(contract);
+		this.routes = () => this._routes();
+		this.start = () => this._start();
+	}
 
-  _response(contract) {
-    return (req, res) => {
-      let body = contract.examples.default.response.body;
-      res.set(contract.response.headers);
-      res.status(contract.response.status).end((typeof body === 'string') ? body : JSON.stringify(body));
-    }
-  }
+	_response(contract) {
+		return (req, res) => {
+			let body = contract.examples.default.response.body;
+			res.set(contract.response.headers);
+			res.status(contract.response.status).end((typeof body === 'string') ? body : JSON.stringify(body));
+		}
+	}
 
-  _routes() {
+	_routes() {
 
-    let that = this;
-    let routes = [];
+		let that = this;
+		let routes = [];
 
-    this.contractReader.contracts().forEach((contract) => {
-      that.app[contract.request.http_method](contract.request.path, that.response(contract));
-      routes += contract.request.http_method.toUpperCase() + ' ' + contract.request.path + '\n';
-    });
+		this.contractReader.contracts().forEach((contract) => {
+			that.app[contract.request.http_method](contract.request.path, that.response(contract));
+			routes += contract.request.http_method.toUpperCase() + ' ' + contract.request.path + '\n';
+		});
 
-    this.app.get('/', (req, res) => { 
-      res.status(200).end(routes);
-    });
-    
-  }
+		this.app.get('/', (req, res) => { 
+			res.status(200).end(routes);
+		});
+		
+	}
 
-  _start() {
-    console.log("API listening on port " + this.port);
-    this.app.listen(this.port, this.routes);
-  }
+	_start() {
+		console.log("API listening on port " + this.port);
+		this.app.listen(this.port, this.routes);
+	}
 
 }
 
--- src/contract-reader.js
+++ src/contract-reader.js
@@ -4,19 +4,19 @@ import Fs from 'fs';
 
 class ContractReader {
 
-  constructor(dir) {
-    this.dir = dir;
-    this.contracts = () => this._contracts();
-    this.fixPath = (contract) => this._fixPath(contract);
-  }
+	constructor(dir) {
+		this.dir = dir;
+		this.contracts = () => this._contracts();
+		this.fixPath = (contract) => this._fixPath(contract);
+	}
 
 	_contracts() {
 		let contracts = [];
 		let that = this;
 		Fs.readdirSync(this.dir).forEach((file) => {
-	    contracts.push(that.fixPath(JSON.parse(Fs.readFileSync(that.dir+'/'+file, 'utf8'))));
-	  });
-	  return contracts;
+			contracts.push(that.fixPath(JSON.parse(Fs.readFileSync(that.dir+'/'+file, 'utf8'))));
+		});
+		return contracts;
 	}
 	
 	_fixPath(contract) {
